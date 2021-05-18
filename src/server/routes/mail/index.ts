f100ed698d90ea4895a1413696002897ddc3c85e
Michael Richardson <mor7991@yahoo.com>
2021-05-17 23:59:27 -0500

    fixed email?

--- src/server/routes/mail/index.ts
+++ src/server/routes/mail/index.ts
@@ -5,8 +5,7 @@ let cors = require('cors');
 import creds from '../../config';
 
 let transport = {
-  service: "yahoo",
-  secure: false,
+  service: "gmail",
   auth: {
     user: creds.email.user,
     pass: creds.email.pass
@@ -29,8 +28,8 @@ router.post('/send', (req: any, res: any, next: any) => {
   let message = req.body.message
   let content = `name: ${name} \n email: ${email} \n message: ${message} `
   let mail = {
-    from: "mor7991@yahoo.com",
-    to: 'mor7991@yahoo.com',  // Change to email address that you want to receive messages on
+    from: email,
+    to: 'mrichardson@mrcreative.dev',  // Change to email address that you want to receive messages on
     subject: 'New Message from Contact Form',
     text: content
   }
