960ea9d82bf23e07d512a979a2ce5db99b835568
Michael Richardson <mor7991@yahoo.com>
2021-05-17 23:35:11 -0500

    maybe fixed email?

--- src/server/routes/mail/index.ts
+++ src/server/routes/mail/index.ts
@@ -6,6 +6,7 @@ import creds from '../../config';
 
 let transport = {
   service: "yahoo",
+  secure: false,
   auth: {
     user: creds.email.user,
     pass: creds.email.pass
@@ -27,13 +28,13 @@ router.post('/send', (req: any, res: any, next: any) => {
   let email = req.body.email
   let message = req.body.message
   let content = `name: ${name} \n email: ${email} \n message: ${message} `
-
   let mail = {
-    from: email,
+    from: "mor7991@yahoo.com",
     to: 'mor7991@yahoo.com',  // Change to email address that you want to receive messages on
     subject: 'New Message from Contact Form',
     text: content
   }
+  console.log("created message");
 
   transporter.sendMail(mail, (err: any, data: any) => {
     if (err) {
